f8021b8a20764fd5cc12209f2b79547ca6f45fc3
Adrian Abraham <adrian@lucius.(none)>
2010-09-15 15:33:53 -0400

    Toggle output every second

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#define F_CPU 1000000
+#define F_CPU 125000
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
@@ -13,10 +13,47 @@
 #define TWOSECS (_BV(WDP2) | _BV(WDP1) | _BV(WDP0))
 #define EIGHTSECS (_BV(WDP3) | _BV(WDP0))
 
+typedef enum {false, true} bool_t;
+typedef enum {UP, DOWN} button_state_t;
+enum {DAY, DILD_WAITING, DILD_ACTIVE, WILD} mode;
+enum {SLOW, FAST} timer;
+
+typedef struct {
+  button_state_t current, new, old;
+  uint32_t time, down_time;
+} button_t;
+
+button_t button1, button2;
+
 inline void wdt_sleep(int);
 inline void wdt_stop(void);
 void power_idle(void);
 void power_down(void);
+inline void turn_on(void);
+inline void turn_off(void);
+void update_button(button_t*, button_state_t);
+
+void fast_timer(void);
+void slow_timer(void);
+
+uint32_t counter;
+uint32_t off_time;
+bool_t active;
+
+void slow_timer() {
+  timer = SLOW;
+ 
+}
+
+void fast_timer() {
+  timer = FAST;
+  wdt_stop();
+
+  TIMSK |= _BV(OCIE1A);
+  TCCR1 = _BV(CTC1) | _BV(CS13) | _BV(CS12); // CTC mode; CLK / 2048 = 61 ticks/sec
+  OCR1A = 61;
+  OCR1C = 61;
+}
 
 inline void wdt_sleep(int mask) {
   WDTCR = _BV(WDIE) | mask; // Enable watchdog interrupt 
@@ -27,40 +64,137 @@ inline void wdt_stop() {
   WDTCR = 0;
 }
 
-void power_idle() {
+void do_sleep(int mode) {
   sleep_enable();
-  set_sleep_mode(SLEEP_MODE_IDLE);
+  set_sleep_mode(mode);
   sleep_cpu();
   sleep_disable();
 }
 
-void power_down() {
-  sleep_enable();
-  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-  sleep_cpu();
-  sleep_disable();
+inline void power_idle() {
+  do_sleep(SLEEP_MODE_IDLE);
+}
+
+inline void power_down() {
+  do_sleep(SLEEP_MODE_PWR_DOWN);
 }
   
+inline void turn_on() {
+  active = true;
+  PORTB |= _BV(PORTB0);
+}
+
+inline void turn_off() {
+  active = false;
+  PORTB &= ~(_BV(PORTB0));
+}
+
+void update_button(button_t *button, uint8_t pin) {
+  button_state_t state = !(PINB & _BV(pin));
+
+  if (state != button->current) {
+    if (button->time + 2 < counter) {
+      button->old = button->current;
+      button->current = state;
+      button->time = counter;
+
+      if (state == DOWN) 
+        button->down_time = counter;
+
+      fast_timer();
+    }
+  }
+}
 
 int main(void) {
   MCUSR = 0; // Reset watchdog status
   wdt_stop();
-  power_all_disable();
-  
-  DDRB = _BV(PORTB0);
-  sei();
-  for(;;) {
-    PORTB = _BV(PORTB0);
-    wdt_sleep(HALFSEC);
-    power_down();
+  //power_all_disable();
 
-    PORTB = 0;
-    wdt_sleep(TWOSECS);
-    power_down();
+  CLKPR = _BV(CLKPCE); // Enable changing the clock prescaler
+  CLKPR = _BV(CLKPS2) | _BV(CLKPS1); // Change the prescaler to 64 (125000)
+
+  DDRB = _BV(DDB0); // B0 is output (vibrator)
+  PORTB = _BV(PORTB1) | _BV(PORTB2); // Pull up B1 and B2 (buttons)
+  mode = DAY;
+
+  PORTB ^= _BV(PORTB0);
+  _delay_ms(1000);
+
+
+  sei();
+
+  fast_timer();
+  for(;;) {
+  }
+
+  for(;;) {
+    update_button(&button1, PINB2);
+    update_button(&button2, PINB1);
+
+    if (button1.current == UP && button1.old == DOWN) {
+      turn_on();
+    }
+
+    if (button2.current == UP && button2.old == DOWN) {
+      turn_off();
+    }
+      
+
+    /*
+    if (off_time < counter) {
+      turn_off();
+    } else {
+      int sleep_time = 0;
+
+      if (mode == DAY) {
+        if (counter > 225) { // About half an hour
+          sleep_time = HALFSEC;
+        }
+      } else if (mode == DILD_WAITING) {
+        if (counter > 2250) { // About 5 hours
+          mode = DILD_ACTIVE;
+          sleep_time = HALFSEC;
+        }
+      } else if (mode == DILD_ACTIVE) {
+        if (counter > 75) { // About 10 minutes
+          sleep_time = HALFSEC;
+        }
+      } else if (mode == WILD) {
+        if (counter > 7) { // About 1 minute
+          sleep_time = QUARTERSEC;
+        }
+      }
+
+      button.old = button.current;
+
+      if (sleep) {
+        counter = 0;
+        turn_on();
+        wdt_sleep(sleep_time);
+        power_down();
+      } else {
+        wdt_sleep(EIGHTSECS);
+        power_down();
+      }
+    }
+    */
+    button1.old = button1.current;
+    button2.old = button2.current;
   }
 
   return 0;
 }
 
 ISR(WDT_vect) {
+  counter++;
 }
+
+ISR(INT0_vect) {
+}
+
+
+ISR(TIMER1_COMPA_vect) {
+  PORTB ^= _BV(PORTB0);
+}
+
