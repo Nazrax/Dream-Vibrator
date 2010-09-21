fddb8cb8bb6069af0d42885ab2cf34e55e9a00ef
Adrian Abraham <aabraham@us.ibm.com>
2010-09-21 12:41:29 -0400

    Use PWM at 50% when in DILD modes

--- io.c
+++ io.c
@@ -2,12 +2,26 @@
 
 inline void turn_on() {
   active = true;
-  PORTB |= _BV(PORTB0);
+
+  if (mode == DILD_ACTIVE || mode == DILD_WAITING) {
+    power_timer0_enable();
+    TCCR0A = _BV(COM0A1) | _BV(WGM01) | _BV(WGM00); // Fast PWM mode, inverting mode
+    TCCR0B = _BV(CS00); // No prescaling
+    OCR0A = 127; // 50% duty cycle
+  } else {
+    PORTB |= _BV(PORTB0);
+  }
 }
 
 inline void turn_off() {
   active = false;
-  PORTB &= ~(_BV(PORTB0));
+  if (mode == DILD_ACTIVE || mode == DILD_WAITING) {
+    TCCR0A = 0;
+    TCCR0B = 0;
+    power_timer0_disable();
+  } else {
+    PORTB &= ~(_BV(PORTB0));
+  }
 }
 
 void update_button(button_t *button, uint8_t pin) {
